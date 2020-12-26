165fa2562c0ccc8d48b3ecded432e2c760cc2219
Linoy Yazdi <linoyasdi@gmail.com>
2020-12-26 18:19:01 +0200

    added exception classes

--- Chess Project/GameException.cpp
+++ Chess Project/GameException.cpp
@@ -1,2 +1,31 @@
 #include "GameException.h"
 
+const char* CausingSelfCheck::what() const
+{
+	return "If you'll move that way, you'll cause a self check!\n";
+}
+
+const char* NoPieceToMOve::what() const
+{
+	return "You are trying to move nothing! there is no piece to move\n";
+}
+
+const char* InvalidMoveToPiece::what() const
+{
+	return "That's an inalid move to this piece!\n";
+}
+
+const char* SamePlace::what() const
+{
+	return "The source place and the destination place are the same place\nYou are not moving the piece!\n";
+}
+
+const char* PieceAtDestPlace::what() const
+{
+	return "There is a piece that is blocking you on your way to the destination place!\n";
+}
+
+const char* InvalidIndex::what() const
+{
+	return "This is an invalid index!\n";
+}
--- Chess Project/GameException.h
+++ Chess Project/GameException.h
@@ -1,9 +1,60 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <exception>
 
-class GameException
+
+class GameException : std::exception
 {
 public:
 	virtual const char* what() const = 0;
+};
+
+
+class MoveException : protected GameException
+{
+public:
+	virtual const char* what() const override = 0;
+};
+
+
+class CausingSelfCheck : protected MoveException
+{
+public:
+	virtual const char* what() const override;
+};
+
+
+class NoPieceToMOve : protected MoveException
+{
+public:
+	virtual const char* what() const override;
+};
+
+
+class InvalidMoveToPiece : protected MoveException
+{
+public:
+	virtual const char* what() const override;
+};
+
+
+class SamePlace : protected MoveException
+{
+public:
+	virtual const char* what() const override;
+};
+
+
+class PieceAtDestPlace : protected MoveException
+{
+public:
+	virtual const char* what() const override;
+};
+
+
+class InvalidIndex : protected MoveException
+{
+public:
+	virtual const char* what() const override;
 };
