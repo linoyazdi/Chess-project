7045d8687536f5eafafb9c3972143e8c2a7617cc
Linoy Yazdi <linoyasdi@gmail.com>
2020-12-28 18:16:18 +0200

    added check exception

---
+++ Chess Project/Knight.cpp
@@ -0,0 +1,9 @@
+#include "Knight.h"
+
+void Knight::move(boardMatrix& boardState, string from, string to, bool checkMove)
+{
+}
+
+Knight::Knight(unsigned color) : Piece(color, color == 0 ? 'N' : 'n')
+{
+}
--- Chess Project/Knight.h
+++ Chess Project/Knight.h
@@ -1,1 +1,10 @@
 #pragma once
+#include "Piece.h"
+
+class Knight : public Piece
+{
+public:
+	virtual void move(boardMatrix& boardState, string from, string to, bool checkMove) override;
+	Knight(unsigned color);
+	Knight() = default;
+};
